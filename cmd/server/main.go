90aae8768db1e470ab6be46ead3dc3da97f6f1ab
Arundhati Das <arundhutidas2000@gmail.com>
2025-10-26 12:52:17 -0400

    Implement RabbitMQ connection in server and update dependencies

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connstr:="amqp://guest:guest@localhost:5672/"
+	done := make(chan os.Signal, 1)
+	conn, err:=amqp.Dial(connstr)
+	
+	if err== nil{
+		fmt.Println("Connection Successfull")
+	} else{
+		fmt.Println("Connection Failed")
+	}
+	defer conn.Close()
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	<-done
+	fmt.Println("Received signal, exiting gracefully...")
 	fmt.Println("Starting Peril server...")
 }

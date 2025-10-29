45706825decc5418642cd20d38fb713e41d65b23
Arundhati Das <arundhutidas2000@gmail.com>
2025-10-29 12:16:40 -0400

    Refactor client and server main functions to establish AMQP connections and handle channel creation

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connstr:="amqp://guest:guest@localhost:5672/"
+	conn, err:=amqp.Dial(connstr)
+	if err== nil{
+		fmt.Println("Connection Successfull")
+	} else{
+		fmt.Println("Connection Failed")
+	}
+	gamelogic.ClientWelcome()
+	func DeclareAndBind(
+		conn *amqp.Connection,
+		exchange,
+		queueName,
+		key string,
+		queueType SimpleQueueType, // an enum to represent "durable" or "transient"
+	) (*amqp.Channel, amqp.Queue, error){
+		ch := conn.Channel()
+		if queueType == "durable"{
+			q:= ch.QueueDeclare(
+				durable: true
+			)
+		}
+		
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -13,13 +15,20 @@ func main() {
 	connstr:="amqp://guest:guest@localhost:5672/"
 	done := make(chan os.Signal, 1)
 	conn, err:=amqp.Dial(connstr)
-	ch,err:=conn.Channel()
-	ch.PublishJSON()
 	if err== nil{
 		fmt.Println("Connection Successfull")
 	} else{
 		fmt.Println("Connection Failed")
 	}
+	ch,err:=conn.Channel()
+	if err== nil{
+		fmt.Println("Channel Creation Successfull")
+	} else{
+		fmt.Println("Channel Creation Failed")
+	}
+	pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
 	defer conn.Close()
 	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
 	<-done

7a4799533d6261afbe2d20f438e07436234b3a49
Arundhati Das <arundhutidas2000@gmail.com>
2025-10-29 14:00:46 -0400

    Refactor client main function and improve DeclareAndBind error handling

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+type SimpleQueueType int8
+
+const(
+	Durable SimpleQueueType = 0
+	Transient SimpleQueueType = 1
+)
+
 
 func main() {
 	fmt.Println("Starting Peril client...")
@@ -16,20 +25,37 @@ func main() {
 	} else{
 		fmt.Println("Connection Failed")
 	}
-	gamelogic.ClientWelcome()
-	func DeclareAndBind(
+	username,err:=gamelogic.ClientWelcome()
+	if err!=nil{
+		fmt.Printf("%s",err)
+	}
+	DeclareAndBind(conn,routing.ExchangePerilDirect,routing.PauseKey+"."+username,routing.PauseKey, 1)
+	
+}
+func DeclareAndBind(
 		conn *amqp.Connection,
 		exchange,
 		queueName,
 		key string,
 		queueType SimpleQueueType, // an enum to represent "durable" or "transient"
-	) (*amqp.Channel, amqp.Queue, error){
-		ch := conn.Channel()
-		if queueType == "durable"{
-			q:= ch.QueueDeclare(
-				durable: true
-			)
-		}
 		
+	) (*amqp.Channel, amqp.Queue, error){
+		ch,err := conn.Channel()
+		if err!= nil{
+			return ch,amqp.Queue{},err
+		}
+		var q amqp.Queue
+		switch queueType {
+			case 0:
+				q,err= ch.QueueDeclare("amqpQueue",true,false,false,false,nil)
+			case 1:
+				q,err= ch.QueueDeclare("amqpQueue",false, true,true, false, nil)
+			default:
+				return ch, amqp.Queue{}, nil
+		}
+		if err!=nil{
+			return ch, amqp.Queue{}, err
+		}
+		ch.QueueBind("amqpQueueBind",key,exchange,false,nil)
+		return ch, q, nil
 	}
-}
